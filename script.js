f35d44f6ec01300658278f09f25234f067a6bba7
pheuthai <info.pheuthai@gmail.com>
2024-07-23 08:37:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
         preloader.style.display = 'none';
     });
 });
+
+var header = $('.header');
+
+$(window).scroll(function(e){
+    if(header.offset().top !== 0){
+        if(!header.hasClass('shadow')){
+            header.addClass('shadow');
+        }
+    }else{
+        header.removeClass('shadow');
+    }
+});
